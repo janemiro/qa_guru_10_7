b953aa23412c811187ea64055bd99ed7549125de
Evgeniia Miroshina <jane.miroshina@gmail.com>
2022-02-11 12:44:34 +0300

    added zip and tests

--- src/test/java/janemiro/FilesTest.java
+++ src/test/java/janemiro/FilesTest.java
@@ -3,82 +3,55 @@ package janemiro;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.codeborne.pdftest.assertj.Assertions.assertThat;
 
 public class FilesTest {
-    @Test
-    @DisplayName("Загрузка файла по относительному пути (рекомендуется!)")
-    void filenameIsVisibleAfterUploadAction() {
-        open("http://file.karelia.ru/");
-        $("input[type='file']").uploadFromClasspath("img.png");
-        $("#file_submit").click();
-        $(".filelist-wrapper").shouldHave(text("Всего: 1 файл общим размером 0 байт\n" + "Загрузка завершена"));
-
-    }
 
     @Test
-    @DisplayName("Работа с PDF и проверка количества страниц")
-    void pdfFileIsDownloadedAndAmountOfPagesIsChecked() throws IOException {
-        open("https://digma.ru/support/cert/");
-        File pdf = $(byText("№ ТС С-GB.АЛ16.В.24384")).download();
-        PDF parsedPdf = new PDF(pdf);
-        assertEquals(2, parsedPdf.numberOfPages);
-    }
+    void ZipTest() throws Exception {
+        ZipFile zipFile = new ZipFile("src/test/resources/files.zip");
 
-    @Test
-    @DisplayName("Работа с PDF и проверка автора")
-    void pdfFileIsDownloadedAndAuthorIsChecked() throws IOException {
-        open("https://junit.org/junit5/docs/current/user-guide/");
-        File pdf = $(byText("PDF download")).download();
-        PDF parsedPdf = new PDF(pdf);
-        assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein",
-                parsedPdf.author);
-    }
+        //Check pdf
 
-    @Test
-    @DisplayName("Работа с XLS и проверка значения")
-    void xlsFileIsDownloadedAndValueIsChecked() throws IOException {
-        open("https://ckmt.ru/price-download.html");
-        File file = $$("a[href*='TehresursPrice']")
-                .find(text("Скачать"))
-                .download();
+        ZipEntry pdfEntry = zipFile.getEntry("PDF doc.pdf");
+        try (InputStream stream = zipFile.getInputStream(pdfEntry)) {
+            PDF parsed = new PDF(stream);
+            assertThat(parsed.text).contains("Предложение Фабрики");
+        }
 
-        XLS parsedXls = new XLS(file);
-        boolean checkPassed = parsedXls.excel
-                .getSheetAt(0)
-                .getRow(296)
-                .getCell(0)
-                .getStringCellValue()
-                .contains("Г В О З Д И ");
+        //Check xls
 
-        assertTrue(checkPassed);
+        ZipEntry xlsEntry = zipFile.getEntry("XLSX doc.xls");
+        try (InputStream stream = zipFile.getInputStream(xlsEntry)) {
+            XLS parsed = new XLS(stream);
+            assertThat(parsed.excel.getSheetAt(0)
+                    .getRow(298)
+                    .getCell(0)
+                    .getStringCellValue())
+                    .isEqualTo("Г В О З Д И ");
+        }
 
-    }
+        //Check csv
 
-    @Test
-    @DisplayName("Работа с CSV, проверка количества строк")
-    void csvFileIsDownloadedAndSmthIsChecked() throws IOException, CsvException {
-        ClassLoader classLoader = this.getClass().getClassLoader();
-        try (InputStream is = classLoader.getResourceAsStream("csv.csv");
-             Reader reader = new InputStreamReader(is)) {
-            CSVReader csvReader = new CSVReader(reader);
-
-            List<String[]> strings = csvReader.readAll();
-            assertEquals(3, strings.size());
-
+        ZipEntry csvEntry = zipFile.getEntry("CSV doc.csv");
+        try (InputStream stream = zipFile.getInputStream(csvEntry)) {
+            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+            List<String[]> list = reader.readAll();
+            assertThat(list).hasSize(3)
+                    .contains(
+                            new String[]{"Cat", "Kitty"}
+                    );
         }
     }
+
 
 
 }
